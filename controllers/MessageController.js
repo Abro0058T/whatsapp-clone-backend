1f9fec63a0190188f5f596ae1820f111c6d08322
AbhiAtiUttam <Abhi@glocalize.net>
2024-07-28 13:32:37 +0530

    added message get contorller

--- controllers/MessageController.js
+++ controllers/MessageController.js
@@ -1,4 +1,4 @@
-import getPrismaInstance from "../utils/PrismaClient"
+import getPrismaInstance from "../utils/PrismaClient.js"
 
 export const addMessage = async(req, res, next) => {
     try{
@@ -6,7 +6,7 @@ export const addMessage = async(req, res, next) => {
         const {message,from,to}=req.body;
         const getUser= onlineUsers.get(to);
         if(message && from && to){
-            const newMessage = await prisma.message.create({
+            const newMessage = await prisma.messages.create({
                 data:{
                     message,
                     sender:{connect:{id:parseInt(from)}},
@@ -14,10 +14,60 @@ export const addMessage = async(req, res, next) => {
                 },
                 include:{sender:true,reciever:true}
             })
-            return res.status(201).send({message:sendMessage});
+            return res.status(201).send({message:newMessage});
         }
         return res.status(400).send("From,to and Message is required.")
     }catch(error){
         next(error)
     }
 }
+
+
+export const getMessages = async (req, res, next) => {
+    try{
+        const prisma=getPrismaInstance()
+        const {from,to}=req.params;
+        
+        const messages =await prisma.messages.findMany({
+            where:{
+                OR:[
+                    {senderId:parseInt(from),recieverId:parseInt(to)},
+                    {senderId:parseInt(to),recieverId:parseInt(from)}
+                ]
+            },
+            orderBy:{
+                id:"asc",
+            }
+        })
+
+        const unreadMessages=[];
+
+
+        messages.forEach = ((message, index) => {
+            if(message.messageStatus !== "read" && message.senderId === parseInt(to)
+            ){
+                messages[index].messageStatus='read';
+                unreadMessages.push(message.id);
+
+            }
+        })
+
+        await prisma.messages.updateMany({
+            id:{
+                id:{in: unreadMessages},
+            },
+            data:{
+                messageStatus:"read",
+            }
+        })
+
+        res.status(200).json({messages})
+    }catch(error){
+        next(error)
+    }
+}
+
+
+
+
+
